cda80c842beccf3d8d0656070082c48341204b2d
Pritesh Mishra <priteshmishra2125@gmail.com>
2025-02-19 11:17:30 +0530

    redis error fixes

--- backend/utils/redisClient.ts
+++ backend/utils/redisClient.ts
@@ -13,6 +13,9 @@
 // export default redisClient;
 
 import { createClient } from 'redis';
+import  dotenv from 'dotenv';
+
+dotenv.config();
 
 const redisClient = createClient({
     url: process.env.REDIS_URL
